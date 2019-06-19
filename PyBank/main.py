b0decb7d190956b6c05096e6109fa99a65843024
Taylor Vickers <taylorvickers@Taylors-MacBook-Pro.local>
2019-06-18 21:56:53 -0500

    message

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,21 @@
 import os
 import csv
 
-pybank_csv = os.path.join("..", "Resources", "web_starter.csv")
+pybank_csv = os.path.join("..", "Resources", "budget_data.csv")
+
+# def financial_analysis(budgetdata):
+    # months = str(budgetdata[0])
+    # pnl = float(budgetdata[1])
+
+with open(pybank_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)
+    data = list(csvreader)
+    row_count = len(data)
+    # for row in csvreader:
+    print(f"Financial Analysis")
+    print(f"----------------------------")
+    print(f"Total Months: {str(row_count)}")
+    print(f"Average Change: ${float()}")
+    print(f"Greatest Increase in Profits: {str()} ({float()})")
+    print(f"Greatest Decrease in Profits: {str()} ({float()})")
