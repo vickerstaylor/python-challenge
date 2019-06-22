f2556f7355fec968bf8676fcd72ba36dbc329891
Taylor Vickers <taylorvickers@Taylors-MacBook-Pro.local>
2019-06-22 16:41:30 -0500

    message

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,19 +3,45 @@ import csv
 
 pybank_csv = os.path.join("..", "Resources", "budget_data.csv")
 
-# def financial_analysis(budgetdata):
-    # months = str(budgetdata[0])
-    # pnl = float(budgetdata[1])
+total = 0
+total_months = 1
+avg_change_array = []
+increase_in_profits = ["", 0]
+decrease_in_profits = ["", 500000000]
 
-with open(pybank_csv, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    next(csvreader)
-    data = list(csvreader)
-    row_count = len(data)
-    # for row in csvreader:
-    print(f"Financial Analysis")
-    print(f"----------------------------")
-    print(f"Total Months: {str(row_count)}")
-    print(f"Average Change: ${float()}")
-    print(f"Greatest Increase in Profits: {str()} ({float()})")
-    print(f"Greatest Decrease in Profits: {str()} ({float()})")
+with open(pybank_csv, newline='') as csvfile:
+
+   csvreader = csv.reader(csvfile, delimiter=',')
+   csv_header = next(csvreader)
+   first_row = next(csvreader)
+   previous = int(first_row[1])
+   total += int(first_row[1])
+
+   for row in csvreader:
+       total_months = total_months + 1
+       total = total + int(row[1])
+       avg_change = int(row[1]) - previous
+       avg_change_array += [avg_change]
+       previous = int(row[1])
+
+       if avg_change > increase_in_profits[1]:
+           increase_in_profits[0] = row[0]
+           increase_in_profits[1] = avg_change
+
+       if avg_change < decrease_in_profits[1]:
+           decrease_in_profits[0] = row[0]
+           decrease_in_profits[1] = avg_change
+
+total_monthly_avg = sum(avg_change_array) / len(avg_change_array)
+
+var1, var2 = increase_in_profits
+var3, var4 = decrease_in_profits
+
+with open("pybank.txt", "w", newline='') as f:
+    f.write(f"Financial Analysis\n")
+    f.write(f"----------------------------\n")
+    f.write(f"Total Months: {str(total_months)}\n")
+    f.write(f"Total: ${total}\n")
+    f.write(f"Average Change: ${round(total_monthly_avg,2)}\n")
+    f.write(f"Greatest Increase in Profits: {var1} (${var2})\n")
+    f.write(f"Greatest Decrease in Profits: {var3} (${var4})")
